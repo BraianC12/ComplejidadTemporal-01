fc4c2ea129cfc313808d56d9418f86c8f1578d4c
Braian <braianenrique89@gmail.com>
2025-08-26 12:19:12 -0300

    prueba

--- tp-01/Program.cs
+++ tp-01/Program.cs
@@ -7,24 +7,7 @@ namespace tp_01
 		public static void Main(string[] args)
 		{
 			ArbolBinario<int> raiz = new ArbolBinario<int>(1);
-            raiz.HijoIzquierdo= new ArbolBinario<int>(2);
-            raiz.HijoDerecho = new ArbolBinario<int>(3);
-            raiz.HijoIzquierdo.HijoIzquierdo = new ArbolBinario<int>(4);
-            raiz.HijoDerecho.HijoIzquierdo = new ArbolBinario<int>(5);
-            raiz.HijoDerecho.HijoDerecho = new ArbolBinario<int>(6);
-            raiz.HijoDerecho.HijoIzquierdo.HijoIzquierdo = new ArbolBinario<int>(7);
-            
-            Console.Write("Recorrido PREORDEN: ");
-            raiz.preorden();
-            
-            Console.Write("\nRecorrido INORDEN: ");
-            raiz.inorden();
-            
-            Console.Write("\nRecorrido POSTORDEN: ");
-            raiz.postorden();
-            
-            
-            
+  
             
 			Console.ReadKey(true);
 		}
