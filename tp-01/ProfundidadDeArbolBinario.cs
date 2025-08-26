d7eb2f78d1f3c09014d12f456c853f70e7340338
Braian <braianenrique89@gmail.com>
2025-08-26 13:53:32 -0300

    primeros codigos del ejercicio 01

---
+++ tp-01/ProfundidadDeArbolBinario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tp_01
+{
+	public class ProfundidadDeArbolBinario
+	{
+		ArbolBinario<int>arbol;
+		
+		public ProfundidadDeArbolBinario(ArbolBinario<int>arbol){
+			this.arbol=arbol;
+		}
+		
+		public void SumarElementosProfundidad(int p){
+			
+		
+		}
+	}
+}
